c6a19cd765dba364c202960ed0edd9a97085b81a
blvckfire <stephenfuc@gmail.com>
2021-11-29 20:32:58 +0100

    Adding project files

---
+++ script.js
@@ -0,0 +1,17 @@
+var css = document.querySelector("h3");
+var colorOne = document.querySelector(".color-one");
+var colorTwo = document.querySelector(".color-two");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+    body.style.background =
+        "linear-gradient(to right, "
+        + colorOne.value
+        + ", "
+        + colorTwo.value
+        + ")";
+}
+
+colorOne.addEventListener("input", setGradient());
+
+colorTwo.addEventListener("input", setGradient());
